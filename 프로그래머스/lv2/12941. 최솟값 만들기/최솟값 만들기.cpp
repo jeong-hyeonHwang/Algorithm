4a203b6e343d0fae614062c34a8fb8ba5404bc6c
jeonghyeon Hwang <96641477+jeong-hyeonHwang@users.noreply.github.com>
2022-12-11 02:31:43 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.09 ms, Memory: 3.88 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.cpp
@@ -0,0 +1,19 @@
+#include<vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> A, vector<int> B)
+{
+    int answer = 0;
+
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+    
+    for(int i=0; i < A.size(); i++) {
+        int bIndex = B.size() - 1 - i;
+        answer += A[i] * B[bIndex];
+    }
+
+    return answer;
+}
